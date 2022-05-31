822d8acc1b1414f5f80228bc55173003a71ab0b4
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-05-31 17:34:00 +0200

    Add new BPNN with neural net

--- models/networks.py
+++ models/networks.py
@@ -211,32 +211,46 @@ def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal'
 ##############################################################################
 # Classes
 ##############################################################################
-
+class NeuralNet(nn.Module):
+    def __init__(self,n1,n2,n3,out_channels):
+        super().__init__()
+        self.fc1 = nn.Linear(64*64*64,n1)
+        self.fc2 = nn.Linear(n1,n2)
+        self.fc3 = nn.Linear(n2,n3)
+        #self.fc5 = nn.Linear(n3,20)
+        self.fc4 = nn.Linear(n3,out_channels)
+    def forward(self,x):
+        x = torch.flatten(x,1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        #x = F.relu(self.fc5(x))
+        x = self.fc4(x)
+        return x
+    
 class BPNN_model(nn.Module):
-    """ Define BPNN model for biological parameter estimation.
-        Added by Rehan JHUBOO
-    """
-    def __init__(self,nof = 16, NB_LABEL = 34):
-        super(BPNN_model,self).__init__()
-        # initialize CNN layers
-        self.conv1 = nn.Conv2d(1,nof,kernel_size = 3,stride = 1, padding = 1)
-        self.conv2 = nn.Conv2d(nof,nof*2, kernel_size = 3, stride = 1, padding = 1)
-        self.conv3 = nn.Conv2d(nof*2,nof*4, kernel_size = 3, stride = 1, padding = 1)
+    def __init__(self,features,out_channels,n1=240,n2=120,n3=60,k1=3,k2=3,k3=3):
+        super(ConvNet,self).__init__()
+        # initialize CNN layers 
+        self.conv1 = nn.Conv2d(1,features,kernel_size = k1,stride = 1, padding = 1)
+        self.conv2 = nn.Conv2d(features,features*2, kernel_size = k2, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(features*2,64, kernel_size = k3, stride = 1, padding = 1)
         self.pool = nn.MaxPool2d(2,2)
         # initialize NN layers
-        self.fc1 = nn.Linear(64*64*64,240)
-        self.fc2 = nn.Linear(240,120)
-        self.fc3 = nn.Linear(120,NB_LABEL)
+        #self.fc1 = nn.Linear(64**3,n1)
+        #self.fc2 = nn.Linear(n1,n2)
+        #self.fc3 = nn.Linear(n2,14)
+        self.neural = NeuralNet(n1,n2,n3,out_channels)
+        # dropout
+        # self.dropout = nn.Dropout(0.25)
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
-        x = torch.flatten(x,1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.neural(x)
+        #x = torch.flatten(x,1)
         return x 
-        
+    
 class GANLoss(nn.Module):
     """Define different GAN objectives.
 
