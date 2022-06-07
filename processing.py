916cbfdf099b65e35b95f6fbedbea2b31586abfb
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-06-07 08:40:55 +0200

    Update processing.py

--- processing.py
+++ processing.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
         # hard-code some parameters for test
         opt_test.num_threads = 0   # test code only supports num_threads = 0
         opt_test.batch_size = 1    # test code only supports batch_size = 1
-        print("in opt:",opt.batch_size,"in opt_test:", opt_test)
+        print("in opt:",opt.batch_size,"in opt_test:", opt_test.batch_size)
         opt_test.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
         opt_test.no_flip = True    # no flip; comment this line if results on flipped images are needed.
         opt_test.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
