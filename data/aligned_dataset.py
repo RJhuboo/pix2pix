7ffbc19b3c64de54e7fa01bc357b7ccf19f9e903
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-28 11:43:51 +0200

    Update aligned_dataset.py

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -55,7 +55,7 @@ class AlignedDataset(BaseDataset):
         transform_params = get_params(self.opt, A.size)
         A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
         B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
-        mask_transform = get_transform(self.opt,convert = True, mask = True)
+        mask_transform = get_transform(self.opt,convert = True, grayscale=True, mask = True)
 
         A = A_transform(A)
         B = B_transform(B)
