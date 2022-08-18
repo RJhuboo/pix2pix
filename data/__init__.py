47b70bea21d35033ac24de8d80b663ec0e965daf
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-08-18 21:23:33 +0200

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -85,7 +85,7 @@ class CustomDatasetDataLoader():
             batch_size=opt.batch_size,
             #shuffle=not opt.serial_batches,
             num_workers=int(opt.num_threads))
-        print("apres dataloader",len(self.datalooder))
+        print("apres dataloader",self.datalooder.size)
 
     def load_data(self):
         return self
