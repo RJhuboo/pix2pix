0e51589667f47fbab9caa8e5fb9d5f06ca77f977
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-08-18 14:05:37 +0200

    Update aligned_dataset.py

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -12,7 +12,7 @@ class AlignedDataset(BaseDataset):
     During test time, you need to prepare a directory '/path/to/data/test'.
     """
 
-    def __init__(self, opt,transform):
+    def __init__(self, opt,transform=None):
         """Initialize this dataset class.
 
         Parameters:
