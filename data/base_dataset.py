e56620809692628221cbbfe9259f3249a59b1ba6
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-08-18 19:03:04 +0200

    Update base_dataset.py

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -105,9 +105,9 @@ def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, conve
         elif params['flip']:
             transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
         if params['flipv']:
-            transform_list.append(transforms.Lambda(lambda: img: __flipvertical(img,params['flipv'])))
+            transform_list.append(transforms.Lambda(lambda img: __flipvertical(img,params['flipv'])))
     if tranform == True:
-        transform_list.append(transforms.Lambda(lambda: img: __rotate(img, params['rotate'])))
+        transform_list.append(transforms.Lambda(lambda img: __rotate(img, params['rotate'])))
     if convert:
         transform_list += [transforms.ToTensor()]
         if mask == False:
